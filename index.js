31089c4015dbd5d9ef89b4e1a478535a992b9d59
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-07-20 22:03:47 -0400

    initial

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const helmet = require('helmet')
+const app = express()
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        reportUri: ['/security-attacks'],
+      },
+    },
+  }),
+)
+app.use(express.static('public'))
+
+const port = 3003
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`)
+})
