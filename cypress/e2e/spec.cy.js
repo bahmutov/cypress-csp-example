20663e4e8cae8c0127d4c140a1df605fd645d544
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-07-20 22:16:00 -0400

    before bold message

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,6 +1,6 @@
 /// <reference types="cypress" />
 
-it('adds new messages one by one', () => {
+it('adds a new message', () => {
   cy.visit('/')
   cy.contains('button', 'Send').should('be.disabled')
   cy.get('#message').type('Hello')
@@ -8,4 +8,13 @@ it('adds new messages one by one', () => {
   cy.get('#messages li').should('have.length', 1).and('have.text', 'Hello')
   cy.get('#message').should('have.value', '')
   cy.contains('button', 'Send').should('be.disabled')
+})
+
+it.only('adds a bold message', () => {
+  cy.visit('/')
+  cy.get('#message').type('<b>Important</b>')
+  cy.contains('button', 'Send').click()
+  cy.get('#messages li b')
+    .should('have.length', 1)
+    .and('have.text', 'Important')
 })
